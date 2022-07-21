f6257dae34c20fe367c86f7770add4682080382c
TanLeblebici <75142903+TanLeblebici@users.noreply.github.com>
2022-07-22 02:19:40 +0300

    Add files via upload

---
+++ FlagGuess/FlagGuess/FlagGuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuessApp.swift
+//  FlagGuess
+//
+//  Created by Tan Leblebici on 22.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlagGuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
